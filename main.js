99c6165361d423d0b8f86bfac9bc06e251cad703
David Bentley <david.bentley95@gmail.com>
2026-01-24 06:51:44 -0800

    Implement Node class

---
+++ main.js
@@ -0,0 +1,7 @@
+class Node {
+    constructor(value = null, leftChild = null, rightChild = null){
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+}
