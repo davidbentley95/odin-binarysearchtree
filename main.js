39fc1a6250140277ef4b1631e5f273e874da987e
David Bentley <david.bentley95@gmail.com>
2026-01-24 18:29:25 -0800

    Implment build tree function

--- main.js
+++ main.js
@@ -8,11 +8,25 @@ class Node {
 
 class Tree {
     constructor(arr) {
-        this.root = buildTree(arr); 
+        const cleanArr = [...new Set(array)].sort((a,b) => a - b);
+        this.root = this.buildTree(cleanArr); 
     }
 
     buildTree(array) {
-        const cleanArr = [...new Set(array)].sort((a,b) => a - b);
+        if(array.length === 0) {
+            return null;
+        }
+
+        const midPoint = Math.floor(array.length / 2);
+        const root = new Node(array[midPoint]); 
+
+        const leftHalf = array.slice(0, mid);
+        const rightHalf = array.slide(mid + 1);
+
+        root.leftChild = this.buildTree(leftHalf);
+        root.rightChild = this.buildTree(rightHalf);
+
+        return root; 
 
     }
 }
