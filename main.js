a32166c273abb138333916f629150a558ad54c52
David Bentley <david.bentley95@gmail.com>
2026-01-26 07:09:16 -0800

    Implement insert function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ class Node {
 
 class Tree {
   constructor(arr) {
-    const cleanArr = [...new Set(array)].sort((a, b) => a - b);
+    const cleanArr = [...new Set(arr)].sort((a, b) => a - b);
     this.root = this.buildTree(cleanArr);
   }
 
@@ -20,8 +20,8 @@ class Tree {
     const midPoint = Math.floor(array.length / 2);
     const root = new Node(array[midPoint]);
 
-    const leftHalf = array.slice(0, mid);
-    const rightHalf = array.slide(mid + 1);
+    const leftHalf = array.slice(0, midPoint);
+    const rightHalf = array.slice(midPoint + 1);
 
     root.leftChild = this.buildTree(leftHalf);
     root.rightChild = this.buildTree(rightHalf);
@@ -34,13 +34,49 @@ class Tree {
       if (node === null) {
         return;
       }
-      if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      if (node.rightChild !== null) {
+        prettyPrint(
+          node.rightChild,
+          `${prefix}${isLeft ? "│   " : "    "}`,
+          false,
+        );
       }
-      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-      if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+      if (node.leftChild !== null) {
+        prettyPrint(
+          node.leftChild,
+          `${prefix}${isLeft ? "    " : "│   "}`,
+          true,
+        );
       }
     };
+  }
+
+  insert(value) {
+    
+    if(this.root === null) {
+      this.root = new Node (value);
+      return this.root;
+    }
+    
+    let currentNode = this.root;
+
+    while (true) {
+      if (currentNode.value === value) {
+        return;
+      }
+
+      if(currentNode.value > value && currentNode.leftChild !== null) {
+        currentNode = currentNode.leftChild;
+      }else if(currentNode.value > value && currentNode.leftChild === null) {
+        currentNode.leftChild = new Node(value);
+        return;
+      }else if(currentNode.value < value && currentNode.rightChild !== null) {
+        currentNode = currentNode.rightChild;
+      }else if(currentNode.value < value && currentNode.rightChild === null){
+        currentNode.rightChild = new Node (value);
+        return;
+      }
+    }
   }
 }
